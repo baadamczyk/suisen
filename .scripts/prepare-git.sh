e6b3dfd02f3d43155b9bef6571b8737f07340cd5
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2020-05-21 23:15:42 +0200

    fixup! Aligned template project structure to fit the needs

--- .scripts/prepare-git.sh
+++ .scripts/prepare-git.sh
@@ -4,4 +4,4 @@ echo ">>> Configuring git credentials"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default simple
-git remote set-url origin https://${GITHUB_TOKEN}@github.com/<github_organization>/<github_repository>.git
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/baadamczyk/suisen.git
